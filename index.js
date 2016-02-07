753ea67a5e0007f0bef5998c4c199142a341f48c
Charlie Greenman <charie@razroo.com>
2016-02-07 18:01:31 -0500

    transfer app over to an express application

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-var http = require('http');
-var myServer = http.createServer(function(request,response){
-    response.writeHead(200, {"Content-Type" : "text/html"});
-    response.write("<b>Hello World</b>");
-    response.end();
-}); //creates a server
+var express = require('express');
+var app = express();
 
-myServer.listen(3000);
+app.get('/', function(req, res){
+  res.send('<h1> Hello </h1> Express');
+});
 
-console.log("go to http://localhost:3000 on your browser");
+var server = app.listen(3000, function(){
+  console.log('Listening on port 300');
+});
+
+// var http = require('http');
+// var myServer = http.createServer(function(request,response){
+//     response.writeHead(200, {"Content-Type" : "text/html"});
+//     response.write("<b>Hello World</b>");
+//     response.end();
+// }); //creates a server
+//
+// myServer.listen(3000);
+//
+// console.log("go to http://localhost:3000 on your browser");
